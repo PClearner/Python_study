85193b420b07de868ab395217bb0abe1e7a1cf36
PClearner <251847423@qq.com>
2024-05-03 22:06:12 +0800

    achieve section2

---
+++ section2/comment.py
@@ -0,0 +1,6 @@
+#say hello to everybody
+print("hello world!")
+
+#test
+
+#I don't know what can I say
---
+++ section2/number.py
@@ -0,0 +1,29 @@
+print(2+3)
+print(3-2)
+
+print(3 ** 2)
+
+print(3 ** (1 + 1))
+
+print(0.1 + 0.1)
+print(2 * 0.2)
+
+print(4/2) # it is always float type
+
+weath = 14_00_00
+print(weath)
+
+x,y,z = 0,1,2
+
+print(f"x = {x} ,y = {y} ,z = {z}")
+
+MAX_INT = 5000 # we always use upper alphet as constant
+
+#test
+print(4+4)
+print(2*4)
+print(64/8)
+print(12-4)
+
+favorite_number = 7
+print(f"my favorite number is {favorite_number}")
