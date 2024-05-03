abf0d8849853b4684b7aff4fb97f214b2a717a6e
PClearner <251847423@qq.com>
2024-05-03 23:52:02 +0800

    complete section3

---
+++ section3/management_list.py
@@ -0,0 +1,38 @@
+cars = ['bmw','audi','toyota','subaru']
+cars.sort()
+print(cars)
+
+cars.sort(reverse=True)
+print(cars)
+
+cars = ['bmw','audi','toyota','subaru']
+print(sorted(cars))
+print(cars)
+print(sorted(cars,reverse=True))
+
+cars = ['bmw','audi','toyota','subaru']
+print(cars)
+cars.reverse()
+print(cars)
+
+print(len(cars))
+
+#test
+travel = ['ammerica','chinese','britain','france']
+print(travel)
+print(sorted(travel))
+print(sorted(travel,reverse=True))
+print(travel)
+travel.reverse()
+print(travel)
+travel.reverse()
+print(travel)
+travel.sort()
+print(travel)
+travel = ['ammerica','chinese','britain','france']
+travel.sort(reverse=True)
+print(travel)
+
+print(len(travel))
+
+#practice3.10: this operation i have achieved in past code
