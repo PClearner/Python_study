c609c5744f7729e2cdd475054748a805ecf6aa7f
PClearner <251847423@qq.com>
2024-05-03 22:34:42 +0800

    complete section3/list.py

---
+++ section3/list.py
@@ -0,0 +1,21 @@
+bicycles = ["trek", "cannon" ,"red" ,"blue"]
+print(bicycles)
+
+print(bicycles[0])
+print(bicycles[0].title())
+
+print(bicycles[1])
+print(bicycles[-1])
+print(bicycles[-2])     # we can find members from the end of this list
+
+print(f"my first bicycle was {bicycles[0].title()}")
+
+#test
+names = ['Albert','Alice','Chandeler','Ross']
+for member in names:
+    print(member.title())
+
+for member in names:
+    print(f"Hello {member.title()}, wish you to have a good day")
+
+#practice 3.3 I don't want to do it,it is boring
